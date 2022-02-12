b5aa01261b8c6434a5b64b777965d1feaf84f837
Graham Hall <ghall89@me.com>
2022-02-12 15:20:58 +0000

    get clues from external json

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const clueSpan = document.querySelector('#clue');
+const countSpan = document.querySelector('#count');
 const answerInput = document.querySelector('#answer');
 const answerBtn = document.querySelector('#btn');
 
+// global question object
 const question = {
-	clue: 'Brine-cured salmon that is lightly smoked.',
-	answer: 'lox'
+	clue: '',
+	answer: ''
 };
 
-clueSpan.innerText = question.clue;
+const getClue = async () => {
+	const rsp = await fetch('./clues.json');
+	const data = await rsp.json();
 
+	question.clue = data[0].clue;
+	question.answer = data[0].answer;
+
+	clueSpan.innerText = question.clue;
+	countSpan.innerText = `${question.answer.length} Letters`;
+};
+
+getClue();
+
+// button click logic
 answerBtn.addEventListener('click', () => {
+	if (answerBtn.innerText === 'Next Clue') {
+		alert('A thing should happen...');
+		return;
+	}
+
+	// determine if answer is correct
+	// & display feedback
 	if (answerInput.value.toLowerCase() === question.answer) {
-		alert('Right!');
+		answerInput.setAttribute('aria-invalid', 'false');
+		answerBtn.innerText = 'Next Clue';
 	} else {
-		alert('Wrong!');
+		answerInput.setAttribute('aria-invalid', 'true');
 	}
 });
